282aad8418492600391f8b238980233356fe9c4b
Ramiro Núñez <ramirosebastiann@gmail.com>
2025-08-03 16:28:23 -0300

    .

--- proyecto3_app/urls.py
+++ proyecto3_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('hola-mundo/', views.hola_mundo, name='hola-mundo')
+        path('hola/', views.hola_mundo, name='hola')
 ]
--- proyecto3_app/views.py
+++ proyecto3_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hola_mundo(request):
+def hola(request):
     return HttpResponse("Hola Juan Carlos")
